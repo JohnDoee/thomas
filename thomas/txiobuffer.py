a746583394fe7e4b93b5e3b1cba300acaea790bf
Anders Jensen <johndoee@tidalstream.org>
2018-08-24 21:28:31 +0200

    removed comment

--- thomas/txiobuffer.py
+++ thomas/txiobuffer.py
@@ -11,40 +11,6 @@ from twisted.internet import defer, threads
 
 logger = logging.getLogger(__name__)
 
-# class TwistedIOBuffer(object):
-#     buffer_fill_increments = 5 * 1024 * 1024
-
-#     def __init__(self, fileObject):
-#         self.fileObject = fileObject
-#         self.data = BytesIO()
-#         self.data_lock = defer.DeferredLock()
-
-#     @defer.inlineCallbacks
-#     def fill_buffer(self):
-#         current_buffer_obj = self.data
-#         data = yield threads.deferToThread(fileObject.read, self.buffer_fill_increments)
-
-#         yield self.data_lock.acquire()
-
-#         if current_buffer_obj == self.data:
-#             # we can avoid locking while reading
-#             # and not create a race condition with "seek"
-#             current_pos = self.data.tell()
-#             self.data.seek(0, SEEK_END)
-#             self.data.write(data)
-#             self.data.seek(current_pos)
-
-#         yield self.data_lock.release()
-
-#     @defer.inlineCallbacks
-#     def seek(self, offset, whence=os.SEEK_SET):
-#         yield self.data_lock.acquire()
-
-#         self.data = BytesIO()
-#         self.fileObject.seek(offset, whence)
-
-#         yield self.data_lock.release()
-
 
 class TwistedIOBuffer(object):
     def __init__(self, fileObject):
